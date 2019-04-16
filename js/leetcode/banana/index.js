e554e289dc1b36d603afbb7ad2105d3f6933c1a1
bingozwbb <1148809866@qq.com>
2019-04-16 17:05:10 +0800

    拍照

---
+++ js/leetcode/banana/index.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @param {number[]} piles
+ * @param {number} H
+ * @param {number} mid
+ * @return {boolean} 
+ */
+function canEatAllBananas(piles, H, mid){
+    // 一定是H 小时
+    let h = 0;
+    // 吃 
+    for (let pile of piles){
+        h += Math.ceil(pile / mid);
+    }
+    return h <= H;
+}
+/**
+ * 
+ * @param {number[]} piles
+ * @param {numeber} H
+ * @return {number} 
+ */
+function minEatingSpeed () { 
+    let lo = 1;
+    let hi = Math.max(...piles);
+    console.log(hi);
+    while(lo <= hi) {
+        // lo尝试
+        let mid = lo + ((hi-lo) >> 1);
+        console.log('-----', mid);
+        // m 正好可以？
+        if (canEatAllBananas(piles, H, m)){
+            hi = mid - 1;
+        }else {
+            lo = mid + 1; // 将最小值改为中间值 +1
+        }
+    }
+    return lo;
+}
+console.log(minEatingSpeed([3, 6, 7, 11], 8));
+
+// console.log(canEatAllBananas([3,6,7,11], 8, 4));
