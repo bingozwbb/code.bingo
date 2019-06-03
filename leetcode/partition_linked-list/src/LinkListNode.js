e5931fb680c8364e9e32fcdd0092799584c0c831
bingozwbb <1148809866@qq.com>
2019-06-03 23:23:03 +0800

    更新

---
+++ leetcode/partition_linked-list/src/LinkListNode.js
@@ -0,0 +1,9 @@
+import LinkedList from "./LinkedList";
+class LinkedListNode {
+    constructor(val) {
+        // 指针？ 
+        this.val = val;
+        this.next = null;
+    }
+}
+export default LinkedListNode
