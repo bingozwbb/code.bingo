79e81e7146c284b96bec04215d1237e1794bf516
bingozwbb <1148809866@qq.com>
2019-06-11 22:26:38 +0800

    VUE

---
+++ VUE/vue-textscroll/vue-textscroll/src/components/view/demo2/child.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">{{msg}}
+        <child @wn="getWn"></child>
+    </div>
+</template>
+<script>
+import child from ''
+export default {
+    name: 'demoChild2',
+
+    data() {
+        return {
+            
+        }
+    },
+    components: {
+
+    }
+}
+</script>
---
+++ VUE/vue-textscroll/vue-textscroll/src/components/view/demo2/father.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <button @wn="getwn">ok</button>
+    </div>
+</template>
+<script>
+import child from './child.vue'
+export default {
+    name: 'demo2',
+    data() {
+        return {
+            message: "我依然很帅"
+        }
+    },
+    methods: {
+        sendMsg () {
+            this,$emit('wn', this.message)
+        }
+    }
+}
+</script>
